b262d8c4b43cfb3306f24977cdba983d66fbafaa
Javier Carrion <javier.carrion@designinteractive.net>
2020-09-17 22:27:46 -0400

    feat: add accordion component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccordionItem } from './components/accordion/accordion-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+  acccordionItems: AccordionItem[] = [
+    {
+      title: 'Example Uno',
+      content: 'Example Content Uno',
+      isExpanded: false
+    },
+    {
+      title: 'Example Dos',
+      content: 'Example Content Dos',
+      isExpanded: false
+    }
+
+  ]
+
 }
