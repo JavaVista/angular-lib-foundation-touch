425c03792b0e9465441b210ab557de9c7af0ff43
Javier Carrion <javier.carrion@designinteractive.net>
2020-10-11 21:43:01 -0400

    feat: :sparkles: add truncate pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PipeDocumentationComponent } from './pipe-documentation/pipe-documentation.component';
 import { PipesRoutingModule } from './pipes-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { TruncatePipe } from './truncate/truncate.pipe';
 
 
 
 @NgModule({
-  declarations: [PipeDocumentationComponent],
+  declarations: [PipeDocumentationComponent, TruncatePipe],
   imports: [
     CommonModule,
     PipesRoutingModule,
