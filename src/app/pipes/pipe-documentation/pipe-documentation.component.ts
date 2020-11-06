507f48f3ea04bee174ac91fb82c3174f9b284f5f
Javier Carrion <javier.carrion@designinteractive.net>
2020-11-05 22:00:56 -0500

    feat(pipe-documentation.component): :sparkles: add text explanation on how a pipe pure/inpure works

--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class PipeDocumentationComponent { 
   flattenData = [1, 2, [3], [4, [5, 6, [7]]]];
 
+
   modifyFlatten(): void {
     this.flattenData.push(8, 9, 10);
   }
