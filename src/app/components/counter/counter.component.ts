8489b69accf01a595dbc55768411162b33e2ee37
Javi Carrion <suavejavi@gmail.com>
2021-03-07 19:45:06 -0500

    refactor(counter-component): :fire: remove unused code

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -5,15 +5,10 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent {
   @Input() counter: number = 0;
   @Input() min: number = 0;
   @Input() max: number = Number.MAX_SAFE_INTEGER; // Highest number in safe in JS
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   decrement(): void {
     if (this.min < this.counter) {
